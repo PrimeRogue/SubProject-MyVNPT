a69d4558275acdd6d3bfb81e94cb0e1a59929ffd
NguyenLongMoon <longsky0912624119@gmail.com>
2023-11-25 04:10:29 +0700

    bo sung cai dat ung dung va ma gioi thieu trang ca nhan

--- src/screens/mainScreens/Personal.js
+++ src/screens/mainScreens/Personal.js
@@ -141,6 +141,182 @@ import {
           </View>
         </View>
         
+
+<Text
+          style={{
+            fontSize: 18,
+            fontWeight: "bold",
+            marginTop: 30,
+            marginBottom: 20,
+            marginLeft: 15,
+          }}
+        >
+          Quản lý thanh toán
+        </Text>
+        <View
+          style={{
+            margin: 15,
+            backgroundColor: "#fff",
+            borderRadius: 10,
+            padding: 15,
+            marginBottom: 25,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 5,
+              marginBottom: 20,
+              alignItems: "center",
+            }}
+          >
+            <Feather name="credit-card" size={25} color="#787D82" />
+            <Text style={{ fontSize: 15, fontWeight: "bold", color: "#787D82" }}>
+              Nguồn tiền
+            </Text>
+          </View>
+          <View
+            style={{
+              borderRadius: 5,
+              borderWidth: 1,
+              borderColor: "#2B7AFB",
+              width: "40%",
+              height: 150,
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 10,
+              marginBottom: 15,
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                height: 50,
+                width: 50,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#EDEFF4",
+                borderRadius: "50%",
+              }}
+            >
+              <MaterialCommunityIcons name="plus" size={32} color="#3575FF" />
+            </TouchableOpacity>
+  
+            <Text
+              style={{
+                fontSize: 15,
+                textAlign: "center",
+                color: "#2D7BF8",
+                fontWeight: "bold",
+              }}
+            >
+              Thêm liên kết
+            </Text>
+          </View>
+          <View
+            style={{
+              width: "100%",
+              height: 1,
+              backgroundColor: "#D7DAD9",
+              marginBottom: 20,
+            }}
+          ></View>
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 5,
+              alignItems: "center",
+            }}
+          >
+            <MaterialCommunityIcons
+              name="layers-outline"
+              size={25}
+              color="#787D82"
+            />
+            <Text style={{ fontSize: 15, fontWeight: "bold", color: "#787D82" }}>
+              Tiện ích thanh toán
+            </Text>
+            <Feather
+              name="chevron-right"
+              size={25}
+              color="#787D82"
+              style={{ position: "absolute", right: 0 }}
+            />
+          </View>
+        </View>
+
+
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: "bold",
+            marginLeft: 15,
+          }}
+        >
+          Khác
+        </Text>
+        <View
+          style={{
+            margin: 15,
+            backgroundColor: "#fff",
+            borderRadius: 10,
+            padding: 15,
+            marginBottom: 100,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 5,
+              alignItems: "center",
+              borderBottomWidth: 1,
+              borderBottomColor: "#D7DAD9",
+              paddingBottom: 15,
+            }}
+          >
+            <AntDesign name="setting" size={25} color="#787D82" />
+            <Text style={{ fontSize: 15, fontWeight: "bold", color: "#787D82" }}>
+              Cài đặt ứng dụng
+            </Text>
+            <Feather
+              name="chevron-right"
+              size={25}
+              color="#787D82"
+              style={{ position: "absolute", right: 0 }}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 5,
+              alignItems: "center",
+              borderBottomWidth: 1,
+              borderBottomColor: "#D7DAD9",
+              paddingBottom: 15,
+              paddingTop: 15,
+            }}
+          >
+            <AntDesign name="sharealt" size={25} color="#787D82" />
+            <Text style={{ fontSize: 15, fontWeight: "bold", color: "#787D82" }}>
+              Mã giới thiệu
+            </Text>
+            <Feather
+              name="chevron-right"
+              size={25}
+              color="#787D82"
+              style={{ position: "absolute", right: 0 }}
+            />
+          </View>
+          
+
+
+
+
+          
+        </View>
+
+
+
+
       </ScrollView>
     );
   }
